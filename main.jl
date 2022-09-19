3dc88ab43410a66529147c237a093d8844830eed
Joseph Mullins <josephlyonmullins@gmail.com>
2022-09-19 14:18:55 -0500

    Finished writing code to simulate data

--- main.jl
+++ main.jl
@@ -25,22 +25,15 @@ C = rand(5,5)
 θ = 1.
 a = [0.2,0.2,0.2,0.4] #<= order: a_m,a_f,a_g,a_Y
 δ = [0.5,0.5]
+logθ = 0.
 
 #β_1: assume J=2
-λ = 0.9
+λ = [1.,0.9]
 σ_ζ = [0.4,0.4]
 
 
 # function to draw (ψ_0,I_1)
 function initial_draw(π0,μ,Σ)
-    k = 1 + rand()<π0[1] #<- draw the mixture
-    x = rand(MultivariateNormal(μ[k],Σ[k]))
-    return x
-end
-##the conditional here isn't parsing properly 
-
-# function to draw (ψ_0,I_1)
-function initial_draw_temp(π0,μ,Σ)
     if (rand()<π0[1])
         k=1
     else
@@ -62,57 +55,61 @@ function logCES(τ_m,τ_f,g,Y,logΨ_0,δ,a,γ,ρ,logθ)
 end
 
 
-
 function draw_logΨ1(τ_m,τ_f,g,Y,logΨ_0,δ,a,γ,ρ,logθ,σ_η)
     return logCES(τ_m,τ_f,g,Y,logΨ_0,δ,a,γ,ρ,logθ) + rand(Normal(0,σ_η))
 end 
+
+function draw_logΨ1(x,δ,a,γ,ρ,logθ,σ_η)
+    return draw_logΨ1(exp(x[1]),exp(x[2]),exp(x[3]),exp(x[4]),x[5],δ,a,γ,ρ,logθ,σ_η)
+end
+
 ##total human capital investment a child receives as a function of all inputs (investment inputs)
 
-# return the vector:
-function draw_M(τ_m,τ_f,g,Y,logΨ_0,δ,a,γ,ρ,logθ,σ_η,λ,σ_ζ)
-    logΨ = draw_logΨ1(τ_m,τ_f,g,Y,logΨ_0,δ,a,γ,ρ,logθ,σ_η)
-    return  λ*logΨ.+rand(MultivariateNormal([0,0],σ_ζ))
+logΨ1 = draw_logΨ1(x,δ,a,γ,ρ,logθ,σ_η)
+
+# function to return a 2 x 1 vector of measurements, M:
+function draw_M(logΨ,λ,σ_ζ)
+    return  λ*logΨ .+ rand(MultivariateNormal([0,0],σ_ζ))
 end
-##some funky stuff going on here 
+
+M = draw_M(logΨ1,λ,σ_ζ)
 
 #function to draw N observations given parameters
 function draw_data(N,π0,μ,Σ) 
-    dat = Array{Float64, 3}(undef, 5, 1, N)
+    dat = Array{Float64, 2}(undef, 5, N)
     for i in 1:N
-        dat[:,:,i]=initial_draw_temp(π0,μ,Σ)
+        dat[:,i]=initial_draw(π0,μ,Σ)
     end
     return dat
 end
 
-# return N x 4 array of measurements (our M)
-function draw_measurements(N,τ_m,τ_f,g,Y,logΨ_0,δ,a,γ,ρ,logθ,σ_η,λ,σ_ζ) 
-    dat = Array{Float64, 3}(undef, 2, 1, N)
-    for i in 1:N
-        dat[:,:,i]=draw_M(τ_m,τ_f,g,Y,logΨ_0,δ,a,γ,ρ,logθ,σ_η,λ,σ_ζ)
-    end
-    return dat
-end
-
+N = 10000
+dat0 = draw_data(N,π0,μ,Σ)
 
 #N x 2 Array of Skills 
-function draw_skills(N,τ_m,τ_f,g,Y,logΨ_0,δ,a,γ,ρ,logθ,σ_η) 
-    dat = Array{Float64, 3}(undef, 1, 1, N)
+function draw_skills(dat0,δ,a,γ,ρ,logθ,σ_η)
+    N = size(dat0)[2]
+    dat = Array{Float64, 1}(undef, N)
     for i in 1:N
-        dat[:,:,i].=draw_logΨ1(τ_m,τ_f,g,Y,logΨ_0,δ,a,γ,ρ,logθ,σ_η)
-    end
-    return dat
-end
-#same dimensionality issue
-
-
-#N x 4 array of inputs
-#(mother and father time inputs, goods inputs inside the home, childcare input)
-function draw_inputs(N) 
-    dat = Array{Float64, 3}(undef, 4, 1, N)
-    for i in 1:N
-        dat[:,:,i]=rand(Float64,4)
+        @views dat[i] =draw_logΨ1(dat0[:,i],δ,a,γ,ρ,logθ,σ_η)
     end
     return dat
 end
 
+dat1 = draw_skills(dat0,δ,a,γ,ρ,logθ,σ_η);
+@time dat1 = draw_skills(dat0,δ,a,γ,ρ,logθ,σ_η);
+
+# return N x 4 array of measurements (our M)
+function draw_measurements(dat1,dat0,λ,σ_ζ) 
+    datM = Array{Float64, 3}(undef, 2, 2, N)
+    for i in 1:N
+        datM[:,1,i]=draw_M(dat0[5,i],λ,σ_ζ)
+        datM[:,2,i]=draw_M(dat1[i],λ,σ_ζ)
+    end
+    return datM
+end
+
+datM = draw_measurements(dat1,dat0,λ,σ_ζ) 
+
+#next: code up a NLLS criterion, write estimation routine and check that it works
 
