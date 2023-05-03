ff55363934705f265b0bd8a072e4caa52bc51f94
Joseph Mullins <josephlyonmullins@gmail.com>
2023-05-03 11:37:29 -0500

    add age restriction, new results

--- relative_demand.jl
+++ relative_demand.jl
@@ -146,7 +146,7 @@ function demand_moments_stacked!(pars,n,g,R,data,spec)
     # --- 1997 relative demand moments
     it = (n-1)*6 + 1
     R[:] .= 0.
-    if data.prices_observed[it] #&& (data.age[it]<=12) && (data.ind_not_sample[it]==0)
+    if data.prices_observed[it] && (data.age[it]<=12) && (data.ind_not_sample[it]==0)
         calc_demand_resids!(it,R,data,pars)
         resids = view(R,[1])
         g_it = view(g,spec.g_idx_97)
@@ -157,7 +157,7 @@ function demand_moments_stacked!(pars,n,g,R,data,spec)
     it = n*6 
     r_idx = [4,5,3,1]
     R[:] .= 0.
-    if data.prices_observed[it] #&& (data.age[it]<=12) && (data.ind_not_sample[it]==0)
+    if data.prices_observed[it] && (data.age[it]<=12) && (data.ind_not_sample[it]==0)
         calc_demand_resids!(it,R,data,pars)
         resids = view(R,r_idx)
         g_it = view(g,spec.g_idx_02)
